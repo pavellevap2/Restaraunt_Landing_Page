ef7f3d15b479eff34bbad9c9da30342e85e826e6
Your Name <youremail@domain.com>
2018-01-23 21:58:17 +0300

    pochti konec

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,12 +1,7 @@
-let btn=document.getElementById("test");
-function toggleBlocks(){
-    let fitstDisplay=document.querySelector('.first-hidden').style.display;
-    let secondDisplay=document.querySelector('.second-hidden').style.display;
-
-    if((fitstDisplay==="none")&&(secondDisplay==="none")){
-        (document.querySelector('.first-hidden').style.display="block") &&(document.querySelector('.second-hidden').style.display="block")
-        && (btn.style.display='none');
-    }
-    return false
-}
-btn.addEventListener('click',toggleBlocks);
+let xs = [] ;
+xs = [] ;
+let ys = xs ;
+ys = [] ;
+xs.push(1) ;
+console.log(xs);
+console.log(ys);
